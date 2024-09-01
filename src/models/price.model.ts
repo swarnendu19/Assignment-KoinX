99a3123ef6485e47509821f3e6c69d584586198a
Swarnendu <swarnendumaity633@gmail.com>
2024-09-01 15:58:05 +0530

    Task 2: fetching ethereum price in every 10 min

--- src/models/price.model.ts
+++ src/models/price.model.ts
@@ -2,10 +2,19 @@ import mongoose from "mongoose";
 
 //Here I have defined a Price schema and model
 const priceSchema = new mongoose.Schema({
-    currency: String,
-    price: Number,
-    timestamp: { type: Date, default: Date.now },
-  });
+  currency: {
+      type: String,
+      required: true,
+  },
+  price: {
+      type: Number,
+      required: true,
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now,
+  },
+});
 
 export const Price = mongoose.model("Price", priceSchema);
 
